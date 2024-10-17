0519dad0f3f6b074658ceea5cb8641f4ec37fa87
mengkongaun <maun@ucsd.edu>
2024-10-16 18:51:13 -0700

    Dark Mode

--- global.js
+++ global.js
@@ -42,3 +42,37 @@ for (let p of pages) {
 
   nav.append(a);
 }
+// Insert dark mode switch HTML at the start of the body
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+    <label class="color-scheme" style="position: absolute; top: 1rem; right: 1rem; font-size: 80%;">
+      Theme:
+      <select id="color-scheme-selector">
+        <option value="light dark">Automatic</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </label>
+  `
+);
+
+const select = document.querySelector("#color-scheme-selector");
+
+// Function to set the color scheme
+function setColorScheme(scheme) {
+  document.documentElement.style.setProperty('color-scheme', scheme);
+  localStorage.colorScheme = scheme; // Save to localStorage
+}
+
+// Set the initial color scheme from localStorage if it exists
+if ("colorScheme" in localStorage) {
+  select.value = localStorage.colorScheme;
+  setColorScheme(localStorage.colorScheme);
+}
+
+// Event listener for changing the color scheme
+select.addEventListener('input', (event) => {
+  const scheme = event.target.value;
+  setColorScheme(scheme);
+});
